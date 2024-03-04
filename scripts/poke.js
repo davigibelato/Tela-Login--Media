2599ce3a09f797466bdfc858b3272a104bd13604
davigibelato <davi_gibelato@hotmail.com>
2024-03-04 17:30:36 -0300

    Fiz tudo

---
+++ scripts/poke.js
@@ -0,0 +1,24 @@
+function loadpk(){
+    let url = 'https://pokeapi.co/api/v2/pokemon/';
+
+    fetch(url + pesquisa.value.trim())
+    .then((response) => {
+        return response.json();
+    })
+    
+    .then((data) => {
+        console.clear();
+        console.log(data)
+        let pokeName = document.getElementById('nome').innerHTML = data['name'];
+        console.log(pokeName);
+        document.getElementById('numero').innerHTML = data['id'];
+        let img = data['sprites']['front_default'];
+        document.getElementById('pic').setAttribute('src', img)
+    })
+
+    .catch((erro) => {
+        console.log("Erro: " + erro);
+    });
+}
+
+document.getElementById('btn1').onclick = loadpk;
